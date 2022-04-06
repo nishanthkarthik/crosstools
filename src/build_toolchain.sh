77c3297744d489e4dd8e5d591a0472246597dd2a
Karthik Nishanth <nishanthkarthik@live.com>
2022-04-06 07:37:57 +0200

    Update zst extension

--- src/build_toolchain.sh
+++ src/build_toolchain.sh
@@ -9,4 +9,4 @@ WORK_DIR="/build/toolchains/$CONFIG_NAME.d"
 mkdir --parents "$WORK_DIR" && cd "$WORK_DIR"
 cp "$CONFIG" $PWD/.config
 /build/ct-ng-bin/bin/ct-ng build
-tar -I zstd -C $PWD/x-tools -caf /build/$CONFIG_NAME.tar.zstd .
+tar -I zstd -C $PWD/x-tools -caf /build/$CONFIG_NAME.tar.zst .
